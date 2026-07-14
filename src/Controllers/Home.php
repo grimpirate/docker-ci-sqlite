62362fe41f1448a1f9cd5c9fa0a4fe90e0cdeb71
grimpirate <grimpirate_jrs@yahoo.com>
2026-07-14 16:26:53 -0400

    CheckPhpIni is more useful than welcome_message

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -2,10 +2,13 @@
 
 namespace Modules\Master\Controllers;
 
+use CodeIgniter\Security\CheckPhpIni;
+
 class Home extends \App\Controllers\BaseController
 {
 	public function index(): string
 	{
-		return view('Modules\Master\Views\welcome_message');
+		return CheckPhpIni::run(false);
+		//return view('Modules\Master\Views\welcome_message');
 	}
 }
